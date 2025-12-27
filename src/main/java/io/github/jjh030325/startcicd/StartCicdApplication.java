89090fa946a6cd6dc9a99d8333f0bdea445befef
wpqlks7 <wpqlks7@gmail.com>
2025-12-27 18:51:18 +0900

    first commit

---
+++ src/main/java/io/github/jjh030325/startcicd/StartCicdApplication.java
@@ -0,0 +1,13 @@
+package io.github.jjh030325.startcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartCicdApplication.class, args);
+    }
+
+}
